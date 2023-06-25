85558ccc5ea3219e90d055e462ed9aa8bf9d3a99
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-06-25 17:44:58 +0100

    Added routes

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const tasks = require("./routes/tasks");
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  res.send("Server is working");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+//app.get("/api/v1/tasks") - get all the tasks
+//app.post("/api/v1/tasks") - create a new tasks
+//app.get("/api/v1/tasks/:id") - get single
+//app.patch("/api/v1/tasks/:id") - update tasks
+//app.delete("/api/v1/tasks") - delete tasks
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
