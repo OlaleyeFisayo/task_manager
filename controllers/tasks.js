bf977f38efacc86bc2d1b15c6755979105fb9012
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-06-26 13:17:12 +0100

    Update tasks.js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -35,8 +35,17 @@ const updateTask = (req, res) => {
   res.send("update task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with the id : ${taskID}` });
+    }
+    res.status(204);
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
