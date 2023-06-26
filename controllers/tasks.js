013f497169ef958f5f083da71bb73ca8f6ac1f9e
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-06-26 13:33:28 +0100

    Add deleteTask

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -42,7 +42,7 @@ const deleteTask = async (req, res) => {
     if (!task) {
       return res.status(404).json({ msg: `No task with the id : ${taskID}` });
     }
-    res.status(204);
+    res.status(200).json({ task });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
