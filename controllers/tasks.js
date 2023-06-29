512396b3791b6b9f589536415e6a8d434d7ef683
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-06-29 17:56:28 +0100

    Added Error Handling

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require("../models/task");
 const asyncWrapper = require("../middleware/async");
+const {createCustomError, CustomAPIError} = require("../errors/custom-error")
 
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -15,9 +16,7 @@ const getTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID }).exec();
   if (!task) {
-    const error = new Error();
-    error.status(404);
-    return next(error);
+    return next(createCustomError(`No Task with the id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
@@ -26,7 +25,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `No task with the id : ${taskID}` });
+    return next(createCustomError(`No Task with the id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
@@ -38,7 +37,7 @@ const updateTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!task) {
-    return res.status(404).json({ msg: `No task with the id : ${taskID}` });
+    return next(createCustomError(`No Task with the id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,13 @@
+const { CustomAPIError } = require("../errors/custom-error");
+
 //the code below is to handle error responses since we passed them to the next function in the async wrapper
 const errorHandler = (err, req, res, next) => {
-  res.status(err.status).json({ msg: err.message });
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+  return res
+    .status(500)
+    .json({ msg: `Something went wrong, please try again` });
 };
 
 module.exports = errorHandler;
